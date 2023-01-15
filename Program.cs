3ebff9f0ea462a37bd38e38ed1c9a345c5e24419
Ondra907 <ondra907@email.cz>
2023-01-15 14:12:24 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace tempclean
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            titleCw();
+            // Paths
+            //string userPath = Path.Combine(Environment.GetEnvironmentVariable("USERPROFILE"));
+            //string path = $"C:\\Users\\{userPath}\\AppData\\Local\\Temp";
+
+            //string path = Path.Combine(Environment.ExpandEnvironmentVariables("%TEMP%"), Environment.UserName, "example");
+
+            string pathUser = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).FullName;
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                pathUser = Directory.GetParent(pathUser).ToString();
+            }
+
+            string pathTemp1 = $"{pathUser}\\AppData\\Local\\Temp";
+            string pathFetch = $"{pathUser}\\AppData\\Local\\Temp";
+
+            string[] files = Directory.GetFiles(pathTemp1);
+            int fileCount = files.Length;
+
+            string[] folders = Directory.GetDirectories(pathTemp1);
+            int folderCount = folders.Length;
+
+            int count = folderCount + fileCount;
+            int deletedF = 0;
+            int deletedFl = 0;
+            int deleted = 0;
+
+            //bool work = true;
+
+            //while (work)
+            //{
+
+            Console.WriteLine(@"Found [{0}] files and [{1}] folders in [{2}]. 
+
+Press ENTER to delete them.", fileCount, folderCount, pathTemp1);
+            Console.ReadKey();
+
+                try
+                {
+                    foreach (string entry in Directory.EnumerateFileSystemEntries(pathTemp1))
+                    {
+                        try
+                        {
+
+                        if (File.Exists(entry) && !Path.GetExtension(entry).Equals(".tmp", StringComparison.OrdinalIgnoreCase))
+                        {
+                            File.SetAttributes(entry, FileAttributes.Normal);
+                            File.Delete(entry);
+                        }
+
+                        if (File.Exists(entry))
+                            {
+                                using (FileStream stream = new FileStream(entry, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+                                {
+                                    File.Delete(entry);
+                                    deletedF++;
+                            }
+                            }
+                            else if (Directory.Exists(entry))
+                            {
+                                Directory.Delete(entry, true);
+                                deletedFl++;
+                        }
+
+                        }
+
+                        catch (UnauthorizedAccessException)
+                        {
+                            Console.WriteLine("Not enough perms to delete file: " + entry);
+                        }
+
+                        catch (IOException)
+                        {
+                            Console.WriteLine("File" + entry + " is being used by another process, it cannot be deleted.");
+                        }
+
+                    //Console.WriteLine($"Deleted {deleted} / {fileCount}");
+                }
+                deleted = deletedF + deletedFl;
+                Console.Clear();
+                titleCw();
+                    Console.WriteLine(@"
+Deleted:
+
+[{0}] Files
+[{1}] Folders
+
+[{2}] Total
+
+---------------------------------------------------------------------------------------------------------
+
+Press ENTER to close application.", deletedF, deletedFl, deleted);
+                    Console.ReadKey();
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error with deleting files or folders: " + e.Message);
+                }
+            }
+
+        static void titleCw()
+        {
+            Console.WriteLine(@"
+    ████████╗███████╗███╗░░░███╗██████╗░  ░█████╗░██╗░░░░░███████╗░█████╗░███╗░░██╗███████╗██████╗░
+    ╚══██╔══╝██╔════╝████╗░████║██╔══██╗  ██╔══██╗██║░░░░░██╔════╝██╔══██╗████╗░██║██╔════╝██╔══██╗
+    ░░░██║░░░█████╗░░██╔████╔██║██████╔╝  ██║░░╚═╝██║░░░░░█████╗░░███████║██╔██╗██║█████╗░░██████╔╝
+    ░░░██║░░░██╔══╝░░██║╚██╔╝██║██╔═══╝░  ██║░░██╗██║░░░░░██╔══╝░░██╔══██║██║╚████║██╔══╝░░██╔══██╗
+    ░░░██║░░░███████╗██║░╚═╝░██║██║░░░░░  ╚█████╔╝███████╗███████╗██║░░██║██║░╚███║███████╗██║░░██║
+    ░░░╚═╝░░░╚══════╝╚═╝░░░░░╚═╝╚═╝░░░░░  ░╚════╝░╚══════╝╚══════╝╚═╝░░╚═╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝
+
+---------------------------------------------------------------------------------------------------------
+");
+        }
+
+        }
+    }
+//}
